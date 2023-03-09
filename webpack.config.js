267e1d3048268c6fcbd75d548369ca3d437fbec0
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-03-09 11:15:56 +0300

    add webpack & start config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+
+module.exports = {
+	context: path.resolve(__dirname,'src'),
+	mode: 'development',
+	entry: './index.js',
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, 'dist'),
+	}
+}
