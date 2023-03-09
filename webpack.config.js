9310ae57793051f711babefa45c49c05b3baa144
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-03-09 15:28:26 +0300

    save typescript configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,32 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+const createPath = (dirName) => path.resolve(__dirname, dirName);
 
 module.exports = {
 	context: path.resolve(__dirname,'src'),
 	mode: 'development',
-	entry: './index.js',
+	entry: './index.ts',
 	output: {
 		filename: 'index.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
+	resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+        alias: {
+            '@': createPath('src'),
+        }
+    },
+	devtool: "source-map",
+	module: {
+		rules: [
+		  {
+			test: /\.ts?$/,
+			use: 'ts-loader',
+			exclude: /node_modules/,
+		  },
+		],
+	  },
     plugins: [
 		new CleanWebpackPlugin(),
 	]
